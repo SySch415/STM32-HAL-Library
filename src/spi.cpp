3894fed7cae9fdded818d85c1ac30eec346f9b14
Simon <schsim415@gmail.com>
2026-05-18 15:05:42 -0700

    feature: added spi src implement

---
+++ src/spi.cpp
@@ -0,0 +1,36 @@
+#include "../include/spi.hpp"
+#include <cstdint>
+
+hal::SPI::SPI(ID spi_id, PSr prescaler) {
+
+  switch (spi_id) {
+  case hal::SPI::ID::SPI1:
+    spi_ = hal::regs::SPI1;
+    break;
+  case hal::SPI::ID::SPI2:
+    spi_ = hal::regs::SPI2;
+    break;
+  default:
+    while (1) {
+    };
+  }
+
+  spi_->CR1 |= (1 << 2);                               // master selction
+  spi_->CR1 |= (static_cast<uint8_t>(prescaler) << 3); // baud rate ctrl
+  spi_->CR1 |= (1 << 6);                               // spi enable
+  spi_->CR1 |= (1 << 8); // SSI internal slave select
+  spi_->CR1 |= (1 << 9); // SSM software slave management
+}
+
+uint8_t hal::SPI::transfer_data(uint8_t transfer_data) {
+  uint8_t rx;
+  spi_->DR = transfer_data;
+  while (!(spi_->SR & (1 << 0))) { // wait till recieve buffer has data
+  };
+  rx = spi_->DR;
+  while (spi_->SR &
+         (1 << 7)) { // wait for transmission to fiish (busy flag goes to zero)
+  };
+
+  return rx;
+}
